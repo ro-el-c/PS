e6da1105a0334bb9dc52ee38632ffdf23038976a
ro-el-c <96233738+ro-el-c@users.noreply.github.com>
2021-12-20 21:55:14 +0900

    그대로 출력하기

---
+++ Basic/BJ_11718.java
@@ -0,0 +1,15 @@
+// 그대로 출력하기
+
+import java.util.Scanner;
+
+public class BJ_11718 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str;
+
+        while(sc.hasNext()){
+            str = sc.nextLine();
+            System.out.println(str);
+        }
+    }
+}
