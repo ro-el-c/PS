6c005a2ec18d03463ce884e01a5a0c69bf22c70f
ro-el-c <96233738+ro-el-c@users.noreply.github.com>
2021-12-23 03:29:55 +0900

    나머지

---
+++ Basic/BJ_10430.java
@@ -0,0 +1,18 @@
+// 나머지
+import java.util.Scanner;
+
+public class BJ_10430 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A, B, C;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        sc.close();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+}
