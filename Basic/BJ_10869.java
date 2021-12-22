53109a6cde3e6060451c623861243141f3aaaf3e
ro-el-c <96233738+ro-el-c@users.noreply.github.com>
2021-12-23 03:15:32 +0900

    사칙연산

---
+++ Basic/BJ_10869.java
@@ -0,0 +1,18 @@
+// 사칙연산
+import java.util.Scanner;
+
+public class BJ_10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        sc.close();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
