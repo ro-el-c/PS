b21145c065ccecfded18b79172b4741bdc198a4c
ro-el-c <96233738+ro-el-c@users.noreply.github.com>
2021-12-23 01:55:22 +0900

    별 찍기 - 17

---
+++ Basic/BJ_10992.java
@@ -0,0 +1,25 @@
+// 별 찍기 - 16
+import java.util.Scanner;
+
+public class BJ_10992 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N, i, j, k;
+        N = sc.nextInt();
+        sc.close();
+
+        for(i=0; i<N-1; i++){
+            for(j=0; j<N-i-1; j++)
+                System.out.print(" ");
+            System.out.print("*");
+            if(i != 0){
+                for(j=0; j<2*i-1; j++)
+                    System.out.print(" ");
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(j=0; j<2*N-1; j++)
+            System.out.print("*");
+    }
+}
