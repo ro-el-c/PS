9802f99b5a8f236a593c0253d0ab54f8a9398242
GYEONG EUN <96233738+ro-el-c@users.noreply.github.com>
2021-12-25 23:21:00 +0900

    음계 _ 코드 보완 equals 함수 사용

--- Basic/BJ_2920.java
+++ Basic/BJ_2920.java
@@ -9,6 +9,22 @@ import java.util.Scanner;
 public class BJ_2920 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String asc = "1 2 3 4 5 6 7 8";
+        String dsc = "8 7 6 5 4 3 2 1";
+        String s = sc.nextLine();
+
+        /* 문자열 비교 == 아닌 equals 사용.
+           == : 주솟값 비교
+           equals : 값 비교
+        */
+        if(s.equals(asc))
+            System.out.print("ascending");
+        else if(s.equals(dsc))
+            System.out.print("descending");
+        else
+            System.out.print("mixed");
+
+       /*
         int gap=0, a, b;
         String what = "what";
         a = sc.nextInt();
@@ -28,5 +44,6 @@ public class BJ_2920 {
             }
         }
         System.out.print(what);
+        */
     }
 }
