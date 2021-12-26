9915eb8443b0e30011c943f7618297f1960229fb
GYEONG EUN <96233738+ro-el-c@users.noreply.github.com>
2021-12-27 00:09:27 +0900

    Dog Treats

---
+++ Basic/A_BJ_19602.java
@@ -0,0 +1,19 @@
+// Dog Treats
+
+import java.util.Scanner;
+
+public class A_BJ_19602 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int S, M, L;
+        S = sc.nextInt();
+        M = sc.nextInt();
+        L = sc.nextInt();
+        sc.close();
+
+        if(1*S + 2*M + 3*L >= 10)
+            System.out.print("happy");
+        else
+            System.out.print("sad");
+    }
+}
