642671ade5b7f05b30fd712557642bd53c3afe22
ro-el-c <chung3286@naver.com>
2021-12-28 20:24:04 +0900

    직사각형에서 탈출

---
+++ Math/BJ_1085.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int x, y, w, h;
+	int min = 1000;
+
+	cin >> x >> y >> w >> h;
+
+	if (w - x < min)
+		min = w - x;
+	if (x < min)
+		min = x;
+	if (y < min)
+		min = y;
+	if (h - y < min)
+		min = h - y;
+
+	cout << min;
+
+}
