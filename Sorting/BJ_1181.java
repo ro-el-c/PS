daeb742a98c8671e34f4e883562ea68c0b98ece9
ro-el-c <chung3286@naver.com>
2021-12-31 01:22:50 +0900

    단어 정렬

--- Sorting/BJ_1181.java
+++ Sorting/BJ_1181.java
@@ -33,17 +33,17 @@ public class BJ_1181 {
                     return o1.length() - o2.length(); //// 문자열 길이에 대하여 오름차순. o1의 길이가 o2보다 크다면 자리 바꿈
             }
         });
-        /*
+
+        /*  Comparator
             int compare(T o1, T o2) 일 때, return 값의 의미
             양수 : o1이 왼쪽이어야 함
             0    : o1과 o2가 같다
             음수 : o2가 왼쪽이어야 함
 
             따라서, 위 return o1.length() - o2.length() 의 값이 양수, 즉, o1의 길이가 o2보다 클 때, 두 문자열의 자리가 바뀐다.
-
         */
 
-        /*
+        /* 다른 방법
         사전 순으로 정렬 후, 문자열의 길이순으로 다시 정렬
 
         Arrays.sort(arr);
