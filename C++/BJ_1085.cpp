d2e2cf1ba546ccb23de4516d03d50713795da77e
ro-el-c <chung3286@naver.com>
2022-09-06 18:10:08 +0900

    수정

--- C++/BJ_1085.cpp
+++ C++/BJ_1085.cpp
@@ -4,24 +4,23 @@
 using namespace std;
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	int x, y, w, h;
-	int min = 1000;
+    int x, y, w, h;
+    int min = 1000;
 
-	cin >> x >> y >> w >> h;
+    cin >> x >> y >> w >> h;
 
-	if (w - x < min)
-		min = w - x;
-	if (x < min)
-		min = x;
-	if (y < min)
-		min = y;
-	if (h - y < min)
-		min = h - y;
+    if (w - x < min)
+        min = w - x;
+    if (x < min)
+        min = x;
+    if (y < min)
+        min = y;
+    if (h - y < min)
+        min = h - y;
 
-	cout << min;
-
+    cout << min;
 }
