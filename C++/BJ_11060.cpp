de1cd47708b3c089bab0f334026adaa69e1f8b4d
ro-el-c <chung3286@naver.com>
2023-03-31 20:56:47 +0900

    [C++] 점프 점프 - DP

---
+++ C++/BJ_11060.cpp
@@ -0,0 +1,26 @@
+// 점프 점프
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX 100001
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> vt(n), dp(n);
+    for(int i=0; i<n; i++) {
+        cin >> vt[i];
+        dp[i] = MAX;
+    }
+
+    dp[0] = 0;
+    for(int i=0; i<n; i++){
+        for(int j=1; j<=vt[i] && i+j<n; j++){
+            dp[i+j] = min(dp[i] + 1, dp[i+j]);
+        }
+    }
+
+    cout << (dp[n-1]==MAX ? -1 : dp[n-1]);
+}
